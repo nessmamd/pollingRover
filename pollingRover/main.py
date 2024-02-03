8ee7b08a4cf6078dfd5004bc54757ba5cc4ba7cf
nessmamd <113803210+nessmamd@users.noreply.github.com>
2024-02-02 17:42:53 -0700

    Update main.py

--- pollingRover/main.py
+++ pollingRover/main.py
@@ -2,9 +2,10 @@ from motor__init import *
 import threading
 import os
 
+# dont forget to remove the prev section 
+
 sizeS = len(motors_S)
 sizeDC = len(motors)
-CDetected = True
 inputDC = [False]*sizeDC
 inputS = [False]*sizeS
 connectionDC = [False]*sizeDC
@@ -12,37 +13,27 @@ connectionS = [False]*sizeS
 groundDC = [False]*sizeDC
 groundS = [False]*sizeS
 
-def inputVoltage(number, type):
-    try:
-        first = type[number].getVoltage()
-        time.sleep(1)
-        second = type[number].getVoltage()
-        
-        if(type == motors):
-            prev = inputDC[number]
-            if abs(second - first < 1) and not(22 <= second <= 26):
+#when the identifer is 1 then it is a servo, 2 is dc motor
+
+def inputVoltage(number, identifier, theArray):
+        second = theArray[number].getVoltage()
+        print("Debugger statement: it is ", second)
+        if(identifier == 2):
+            if not(22 <= second <= 26):
                 inputDC[number] = False
-        elif(type == motors_S):
-                if prev != inputDC[number]: CDetected = True
-                
-        elif(type == motors_S):
-            prev = inputS[number]
-            if abs(second - first < 1) and not(11.5 <= second <= 12.5):
+            else: 
+                inputDC[number] = True
+
+        elif(identifier == 1):
+            if not(11.5 <= second <= 12.5):
                 inputS[number] = False
-                if prev != inputS[number]: CDetected = True
+            else: 
+                inputS[number] = True
 
-
-    except AttributeError:
-        if type == motors:
-            inputDC[number] = True
-            if prev != inputDC[number]: CDetected = True
-        else:
-            inputS[number] = True
-            if prev != inputS[number]: CDetected = True
 
 # the two other functions you need to see if there is a change or not
-def turnedOnConnectionServo(number):
-    try:
+def turnedOnConnectionServo(number, motorsArray):
+
         prev = connectionS[number]
         if motors_S[number].getAttached():
             print("The Servo motor #%d is attached"% number)
@@ -57,39 +48,34 @@ def turnedOnConnectionServo(number):
                 motors_S[number].openWaitForAttachment(1000)
                 connectionS[number] = False
                 if prev != connectionS[number]: CDetected = True
-            inputVoltage(number, motors_S)
-            groundVoltage(number, motors_S)
+            inputVoltage(number, 1, motorsArray)
+            
+        else: 
+            connectionS[number] = False
+            if prev != connectionDC[number]: CDetected = True
 
-    except AttributeError:
-         connectionS[number] = False
-         if prev != connectionDC[number]: CDetected = Trues
-
-def turnedOnConnectionDC(number):
-    try:
-        prev = connectionDC[number]
+def turnedOnConnectionDC(number, motorsArray):
+        # prev = connectionDC[number]
+        motors = motorsArray
+        print("boolean ", motors[number].getAttached())
         if motors[number].getAttached():
             print("The DC motor #%d is attached"% number)
             if motors[number].getIsOpen():
                 print("The DC motor #%d is open"% number)
                 connectionDC[number] = True
-                if prev != connectionDC[number]: CDetected = True
+                # if prev != connectionDC[number]: CDetected = True
             else:
                 print("The DC motor #%d is not open" % number)
                 motors[number].openWaitForAttachment(1000)
                 connectionDC[number] = False
-                if prev != connectionDC[number]: CDetected = True
-            inputVoltage(number, motors)
-            groundVoltage(number,motors)
-
-    except AttributeError:
-        connectionDC[number] = False
-        if prev != connectionDC[number]: CDetected = True
-
-def groundVoltage(number,volt):
-    x = 1 
-
+                # if prev != connectionDC[number]: CDetected = True
+            inputVoltage(number, 2, motorsArray)
+        else: 
+            connectionDC[number] = False
+            print("It is not attached !!! ")
+            # if prev != connectionDC[number]: CDetected = True
+            
 def printer(): 
-    if CDetected:
         print("DC MOTOR STATS: ")
         for x in range(sizeDC):
             print("DC MOTOR #",x)
@@ -103,63 +89,34 @@ def printer():
             print("Input voltage status: " + ("FAULTY!!!!!!" if inputS[y] == False else "VALID"))
             print("Connection status:" + (" FAILED " if connectionS[y] == False else " SUCCESS"))
         time.sleep(1)
-        CDetected = False 
 
-def pollerSystem():
-    while True:
-        #  clear the terminal
-        os.system('cls' if os.name == 'nt' else 'clear')
+def pollerSystem(initalizedMotors):
+    #first case is does it enter the pollerSystem?
+    print("entering the poller system")
+    # this is the code for debugging
+    turnedOnConnectionDC(1, initalizedMotors)
+    
+    # cant test the servo ones yet because it is not working in the original one  
+    #  turnedOnConnectionServo(1, initalizedMotors)
+    
+    # this is the final code for everything
+    # time.sleep(1)
+    # while True:
+    #     #  clear the terminal
+    #     os.system('cls' if os.name == 'nt' else 'clear')
 
-        threads_servo = [threading.Thread(target=turnedOnConnectionServo, args=(x,)) for x in range(len(motors_S))]
-        for threadS in threads_servo:
-            threadS.start()
+    #     # threads_servo = [threading.Thread(target=turnedOnConnectionServo, args=(x, initalizedMotors)) for x in range(len(motors_S))]
+    #     # for threadS in threads_servo:
+    #     #     threadS.start()
 
-        threads_dc = [threading.Thread(target=turnedOnConnectionDC, args=(y,)) for y in range(len(motors))]
-        for thread in threads_dc:
-            thread.start()
+    #     threads_dc = [threading.Thread(target=turnedOnConnectionDC, args=(y, initalizedMotors)) for y in range(len(motors))]
+    #     for thread in threads_dc:
+    #         thread.start()
 
-        for threadSS in threads_servo:
-            threadSS.join()
+    #     # for threadSS in threads_servo:
+    #     #     threadSS.join()
 
-        for threadd in threads_dc:
-            threadd.join()
+    #     for threadd in threads_dc:
+    #         threadd.join()
 
-        time.sleep(1)
-
-        print("DC MOTOR STATS: ")
-        for x in range(sizeDC):
-            print("DC MOTOR #", x)
-            print("---------------")
-            print("Input voltage status: " + ("FAULTY!!!!!!" if inputDC[x] == False else "Correct"))
-            print("Connection status:"+ (" FAILED " if connectionDC[x] == False else " SUCCESS"))
-
-        for y in range(sizeS):
-            print("Servo MOTOR #", y)
-            print("---------------")
-            print("Input voltage status: " + ("FAULTY!!!!!!" if inputS[x] == False else "Correct"))
-            print("Connection status:" + (" FAILED " if connectionS[x] == False else " SUCCESS"))
-
-        time.sleep(1)
-
-def pollerSystem():
-    CDetected = True
-    while True:
-        #  clear the terminal
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-        threads_servo = [threading.Thread(target=turnedOnConnectionServo, args=(x,)) for x in range(len(motors_S))]
-        for threadS in threads_servo:
-            threadS.start()
-
-        threads_dc = [threading.Thread(target=turnedOnConnectionDC, args=(y,)) for y in range(len(motors))]
-        for thread in threads_dc:
-            thread.start()
-
-        for threadSS in threads_servo:
-            threadSS.join()
-
-        for threadd in threads_dc:
-            threadd.join()
-
-        time.sleep(1)
-        printer()
+    #     printer()
