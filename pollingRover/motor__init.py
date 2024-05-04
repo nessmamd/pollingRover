61e43f5ddbbe3638ed8b5e3bcbe51a3cf30d6205
nessmamd <113803210+nessmamd@users.noreply.github.com>
2024-05-03 22:20:57 -0600

    Update motor__init.py

--- pollingRover/motor__init.py
+++ pollingRover/motor__init.py
@@ -5,7 +5,7 @@ import time
 from Phidget22.Devices.VoltageInput import *
 
 # initalizing everything
-VHubSerial_motors = 697103
+VHubSerial_motors = 697178
 VHubSerial_servo = 697066
 
 leftSideDriveMotors = DCMotor()
@@ -14,22 +14,29 @@ leftWinchMotor = DCMotor()
 bristleMotor = DCMotor()
 
 ch_lsdm = VoltageInput()
-ch_rsdm = VoltageInput()
-ch_lwm = VoltageInput()
-ch_bm = VoltageInput()
+# ch_rsdm = VoltageInput()
+# ch_lwm = VoltageInput()
+# ch_bm = VoltageInput()
 
 ch_lsdm.setDeviceSerialNumber(VHubSerial_motors)
-ch_rsdm.setDeviceSerialNumber(VHubSerial_motors)
-ch_lwm.setDeviceSerialNumber(VHubSerial_motors)
-ch_bm.setDeviceSerialNumber(VHubSerial_motors)
+# ch_rsdm.setDeviceSerialNumber(VHubSerial_motors)
+# ch_lwm.setDeviceSerialNumber(VHubSerial_motors)
+# ch_bm.setDeviceSerialNumber(VHubSerial_motors)
 
-ch_lsdm.setHubPort(0)
-ch_rsdm.setHubPort(5)
-ch_lwm.setHubPort(1)
-ch_bm.setHubPort(2)
-ch_bm.getChannelSubclass()
+ch_lsdm.setHubPort(4)
+# ch_rsdm.setHubPort(5)
+# ch_lwm.setHubPort(1)
+# ch_bm.setHubPort(2)
+
 motors = [leftSideDriveMotors,rightSideDriveMotors,leftWinchMotor,bristleMotor]
-motorsch = [ch_lsdm, ch_rsdm, ch_lwm, ch_bm]
+motorsch = [ch_lsdm]
+
+# for i in motors:
+#     print(f"drive motor attached: {i.getAttached()}")
+
+print("TESTINGGGGGGG")
+for i in motorsch:
+    print(f"voltage sensor attached: {motorsch[0].getAttached()}")
 
 # steppers
 baseMotor = Stepper()
@@ -39,25 +46,25 @@ wristMotor = Stepper()
 wrist1Motor = Stepper()
 wrist2Motor = Stepper()
 
-ch_S_bm = VoltageInput()
-ch_S_sm = VoltageInput()
-ch_S_em = VoltageInput()
-ch_S_wm0 = VoltageInput()
-ch_S_wm1 = VoltageInput()
-ch_S_wm2 = VoltageInput()
+# ch_S_bm = VoltageInput()
+# ch_S_sm = VoltageInput()
+# ch_S_em = VoltageInput()
+# ch_S_wm0 = VoltageInput()
+# ch_S_wm1 = VoltageInput()
+# ch_S_wm2 = VoltageInput()
 
-ch_S_bm.setDeviceSerialNumber(VHubSerial_servo)
-ch_S_sm.setDeviceSerialNumber(VHubSerial_servo)
-ch_S_em.setDeviceSerialNumber(VHubSerial_servo)
-ch_S_wm0.setDeviceSerialNumber(VHubSerial_servo)
-ch_S_wm1.setDeviceSerialNumber(VHubSerial_servo)
-ch_S_wm2.setDeviceSerialNumber(VHubSerial_servo)
+# ch_S_bm.setDeviceSerialNumber(VHubSerial_servo)
+# ch_S_sm.setDeviceSerialNumber(VHubSerial_servo)
+# ch_S_em.setDeviceSerialNumber(VHubSerial_servo)
+# ch_S_wm0.setDeviceSerialNumber(VHubSerial_servo)
+# ch_S_wm1.setDeviceSerialNumber(VHubSerial_servo)
+# ch_S_wm2.setDeviceSerialNumber(VHubSerial_servo)
 
-ch_S_bm.setHubPort(0)
-ch_S_sm.setHubPort(1)
-ch_S_em.setHubPort(2)
-ch_S_wm0.setHubPort(3)
-ch_S_wm1.setHubPort(4)
-ch_S_wm2.setHubPort(5)
+# ch_S_bm.setHubPort(0)
+# ch_S_sm.setHubPort(1)
+# ch_S_em.setHubPort(2)
+# ch_S_wm0.setHubPort(3)
+# ch_S_wm1.setHubPort(4)
+# ch_S_wm2.setHubPort(5)
 motors_S = [baseMotor,shoulderMotor,elbowMotor,wristMotor, wrist1Motor, wrist2Motor]
-motorsch_S = [ch_S_bm, ch_S_sm, ch_S_em, ch_S_wm0, ch_S_wm1, ch_S_wm2]
+# motorsch_S = [ch_S_bm, ch_S_sm, ch_S_em, ch_S_wm0, ch_S_wm1, ch_S_wm2]
